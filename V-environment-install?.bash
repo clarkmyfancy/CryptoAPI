dc3570e796198595eb4c9673c90605b4754deffa
Jack Conrad Clark <clark.jack0@gmail.com>
2021-12-22 16:29:08 -0600

    add reminder-file to make an install script

---
+++ V-environment-install?.bash
@@ -0,0 +1,14 @@
+# this will get everything setup for you to use this program on any computer
+
+# create a virtual environment
+python3 -m venv env
+
+# activate that environment
+source env/bin/activate
+
+# install necessary packages
+# ...
+# ...
+
+# when you're all done
+# run the command inside of the environment "deactivate" and you'll be good to go
