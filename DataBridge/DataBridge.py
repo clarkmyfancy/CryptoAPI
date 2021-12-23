2b47538838f8654b526218ea3b93d0c87d1c79ed
Jack Conrad Clark <clark.jack0@gmail.com>
2021-12-22 20:27:19 -0600

    create layer between the database and code

---
+++ DataBridge/DataBridge.py
@@ -0,0 +1,13 @@
+import json
+
+def getApiKeySecrets():
+    file = open("CoinMarketCapApi/secrets.json")
+    secrets = json.load(file)
+    file.close()
+    return secrets["api-key"]
+
+def getCryptosOfInterest():
+    file = open("Database/CryptosOfInterest.txt", "r")
+    cryptos = file.read()
+    file.close()
+    return cryptos
