d27d793b0e9ad488bea808853637c06791f6550c
Jack Conrad Clark <clark.jack0@gmail.com>
2021-12-22 20:29:50 -0600

    setup and test public hook points

---
+++ PublicInterface?/ApiEndpoints.py
@@ -0,0 +1,49 @@
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+import ast
+import json
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Cryptos(Resource):
+
+    def get(self):
+        file = open("Database/CryptosOfInterest.txt", "r")
+        contents = file.read()
+        file.close()
+        return { "data": contents }, 200
+
+class Users(Resource):
+    
+    def get(self):
+        file = open("Database/Users.txt", "r")
+        contents = file.read()
+        file.close()
+        return contents, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+
+        parser.add_argument('userId', required=True)
+        parser.add_argument('name', required=False)
+
+        args = parser.parse_args()
+
+        file = open("Database/Users.txt", "a")
+        file.write(args['userId'])
+        file.write("\n")
+        file.close()
+        return None, 200
+# return {'data': data}, 200  #
+
+
+
+api.add_resource(Users, '/users')
+api.add_resource(Cryptos, '/cryptos')
+
+
+if __name__ == '__main__':
+    app.run()  # run our Flask app
