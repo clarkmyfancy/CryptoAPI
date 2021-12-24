da3ac4ae22589fe5546ea5a4ffa8bd3a55ab87f6
Jack Conrad Clark <clark.jack0@gmail.com>
2021-12-24 11:53:14 -0600

    add post request to add new crypto ticker

--- PublicInterface/Cryptos.py
+++ PublicInterface/Cryptos.py
@@ -1,9 +1,18 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
-from DataBridge.DataBridge import getCryptosOfInterest
+from DataBridge.DataBridge import getCryptosOfInterest, addToCryptosOfInterest
 
 class Cryptos(Resource):
 
     def get(self):
         contents = getCryptosOfInterest()
         return { "data": contents }, 200
+
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('new-token', required=True)
+
+        args = parser.parse_args()
+        addToCryptosOfInterest(args['new-token'])
+        return None, 200
+# # return {'data': data}, 200  #
