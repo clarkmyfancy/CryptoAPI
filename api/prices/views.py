230603704ecdf61f3c87d84f50ab2545411fd332
Jack Conrad Clark <clark.jack0@gmail.com>
2022-01-31 07:55:47 -0600

    add views for looking at all cryptos and the details of just one

--- api/prices/views.py
+++ api/prices/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Favorite
 
 def index(request):
     cryptos = Favorite.objects.all()
     context = {
-        'cryptos': cryptos.lower()
+        'cryptos': cryptos
     }
     return render(request, 'prices/index.html', context)
 
-def single(request, name):
-    return HttpResponse(name)
+class IndexView(ListView):
+    template_name = 'prices/index.html'
+    context_object_name = 'cryptos'
+
+    def get_queryset(self):
+        return Favorite.objects.all()
+
+class DetailView(DetailView):
+    template_name = 'prices/detail.html'
+    context_object_name = 'crypto'
+
+    def get_object(self):
+        thing = get_object_or_404(Favorite, ticker__exact=self.kwargs['ticker'])
+        return thing
