2fd1db3005e0d836d81024e86a04b79046076e29
Jack Conrad Clark <clark.jack0@gmail.com>
2022-01-31 13:20:41 -0600

    hook up the dashboard app

--- api/api/settings.py
+++ api/api/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'dashboard.apps.DashboardConfig',
     'prices.apps.PricesConfig',
     'django.contrib.admin',
     'django.contrib.auth',
