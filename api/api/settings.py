239c3775775981936f4d82ba27ba6da3e26fbaba
Jack Conrad Clark <clark.jack0@gmail.com>
2022-01-31 20:52:37 -0600

    fix Procfile

--- api/api/settings.py
+++ api/api/settings.py
@@ -112,12 +112,12 @@ USE_TZ = True
 
 
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+# PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 
 
